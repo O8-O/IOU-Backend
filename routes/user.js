a8d54746b7525dd656ccc701b00d93fd0700ef2e
Your Name <you@example.com>
2020-10-02 10:16:05 +0900

    finish sign in

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const models = require('../models');
 const user = require('../util/user');
 
-router.post('/sign_up', (req, res, next) => {
+router.post('/sign_in', (req, res, next) => {
     var body = req.body;
     
     user.encryptPW(body.password, (err, password, saltMade) => {
-
         user.findUserByID(body.id, (err, result) => {
             models.users.create({
                 ID: body.id,
@@ -21,26 +20,7 @@ router.post('/sign_up', (req, res, next) => {
             .catch(err => {
                 return next(err);
             })
-        });
-        
-        // if(user.findUserByID(body.id)){
-        //     models.users.create({
-        //         ID: body.id,
-        //         PW: password,
-        //         email: body.email,
-        //         salt: saltMade
-        //     })
-        //     .then(result => {
-        //         return res.json({"result": true});
-        //     })
-        //     .catch(err => {
-        //         console.log(err);
-        //         return next(err);
-        //     })
-        // }   
-        // else{
-        //     return next(err);
-        // }     
+        }); 
     });    
 });
 
